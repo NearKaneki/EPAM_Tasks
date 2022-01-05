b6ecbf4aa8071f957270088667a201c6a1f76cf4
Вячеслав Охоткин <near55@yandex.ru>
2022-01-05 22:10:28 +0400

    added 1.1.5

---
+++ Task_1/Task_1.1/1.1.5. SUM OF NUMBERS/1.1.5. SUM OF NUMBERS/Program.cs
@@ -0,0 +1,12 @@
+﻿static int needDigits(int x)
+{
+    int sum = 0;
+    for (int i = x; i < 1000; i += x)
+    {
+        sum += i;
+    }
+    return sum;
+}
+int sum = 0;
+int ans = needDigits(3) + needDigits(5);
+Console.WriteLine(ans);
