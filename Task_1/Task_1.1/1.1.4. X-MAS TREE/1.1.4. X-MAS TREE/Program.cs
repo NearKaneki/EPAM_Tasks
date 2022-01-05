15866fa9ce5ec0adf054d7fd7eb6531674d14ebe
Вячеслав Охоткин <near55@yandex.ru>
2022-01-05 22:04:52 +0400

    added 1.1.4

---
+++ Task_1/Task_1.1/1.1.4. X-MAS TREE/1.1.4. X-MAS TREE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write($"n = ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; ++i)
+{
+    for (int j = 0; j < i; ++j)
+    {
+        string temp = new string(' ', n - j - 1);
+        temp += new string('*', j * 2 + 1);
+        Console.WriteLine(temp);
+    }
+    
+}
