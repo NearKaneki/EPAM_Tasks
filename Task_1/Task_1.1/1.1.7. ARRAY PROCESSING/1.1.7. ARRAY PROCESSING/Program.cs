7e174f0e42d9cd4242e6abd4b0728ff03a54380e
Вячеслав Охоткин <near55@yandex.ru>
2022-01-06 14:01:43 +0400

    added 1.1.7

---
+++ Task_1/Task_1.1/1.1.7. ARRAY PROCESSING/1.1.7. ARRAY PROCESSING/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] array = new int[10];
+Random r = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = r.Next(100);
+    Console.Write($"{array[i]} ");
+}
+//int max = 0;
+//int min = 101;
+//for (int i = 0; i < 10; ++i)
+//{
+//    if (array[i] > max)
+//    {
+//        max = array[i];
+//    }
+//    if (array[i] < min)
+//    {
+//        min = array[i];
+//    }
+//}
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (array[i] > array[j])
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+Console.WriteLine($"min = {array[0]}");
+Console.WriteLine($"max = {array[^1]}");
+
+
+
+
+
