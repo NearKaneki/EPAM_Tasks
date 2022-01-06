2471cabd1c0318fd5ade777fee6128174574f4f6
Вячеслав Охоткин <near55@yandex.ru>
2022-01-06 22:13:05 +0400

    added 1.2.1

---
+++ Task_1/Task_1.2/1.2.1. AVERAGES/1.2.1. AVERAGES/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Input string: ");
+string s = new string(Console.ReadLine());
+char[] separators = { '.', ',', '!', '?', ';', ':', '-', ' ' };
+string[] words = s.Split(separators, System.StringSplitOptions.RemoveEmptyEntries);
+int ans = words.Select(x => x.Length).Sum()/words.Count();//округление в меньшую сторону
+Console.WriteLine(ans);
