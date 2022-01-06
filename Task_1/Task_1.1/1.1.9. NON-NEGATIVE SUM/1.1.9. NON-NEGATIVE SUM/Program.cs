bb317e2046cf1cb37d4160d5b9d0b46bcd5b2146
Вячеслав Охоткин <near55@yandex.ru>
2022-01-06 19:41:28 +0400

    added 1.1.9

---
+++ Task_1/Task_1.1/1.1.9. NON-NEGATIVE SUM/1.1.9. NON-NEGATIVE SUM/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+int[] array = new int[10];
+Random r = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = r.Next(-100, 100);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+Console.WriteLine($"SUM = {array.Where(x => x > 0).Sum()}");
