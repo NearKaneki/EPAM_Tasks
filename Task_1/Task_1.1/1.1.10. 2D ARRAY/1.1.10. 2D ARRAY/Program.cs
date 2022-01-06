2bbe8224caf9c155e8d7baef0370fa42479f17f3
Вячеслав Охоткин <near55@yandex.ru>
2022-01-06 19:55:57 +0400

    added 1.1.10

---
+++ Task_1/Task_1.1/1.1.10. 2D ARRAY/1.1.10. 2D ARRAY/Program.cs
@@ -0,0 +1,22 @@
+﻿int[,] array = new int[5, 5];
+Random r = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = r.Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int ans = 0;
+for (int i = 0; i < array.GetLength(0); ++i)
+{
+    int j = i % 2 == 0 ? 0 : 1;
+    for (; j < array.GetLength(1); j += 2)
+    {
+        ans += array[i, j];
+    }
+}
+Console.WriteLine(ans);
