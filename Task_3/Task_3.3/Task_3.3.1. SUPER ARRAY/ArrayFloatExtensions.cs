c404e032303ec8bc73fa1071ecd60203ac46bc71
Вячеслав Охоткин <near55@yandex.ru>
2022-02-11 13:12:56 +0400

    added new Extensions

---
+++ Task_3/Task_3.3/Task_3.3.1. SUPER ARRAY/ArrayFloatExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_3._3._1._SUPER_ARRAY
+{
+    public static class ArrayFloatExtensions
+    {
+        public static void ChangeArray(this float[] array, Func<float, float> func) =>
+            array = array.Select(x => func(x)).ToArray();
+
+        public static float SumNumbers(this float[] array) => array.Sum();
+
+        public static float AverageNumber(this float[] array) => array.Average();
+
+        public static float OftRepeatedItem(this float[] array)
+        {
+            Dictionary<float, int> dict = new Dictionary<float, int>();
+            foreach (var item in array)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    ++dict[item];
+                    continue;
+                }
+                dict.Add(item, 1);
+            }
+            return dict.Where(x => x.Value == dict.Max(x => x.Value)).FirstOrDefault().Key;
+        }
+    }
+}
